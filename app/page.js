4681acd80aefd712927ba2c5132fd5847bfa9d1e
loisemusungu <mwongelimusungu@gmail.com>
2024-07-30 17:10:37 +0300

    added a list of items

--- app/page.js
+++ app/page.js
@@ -1,13 +1,22 @@
 import { Box } from '@mui/material';
 
+const item = [
+  'tomato',
+  'potato',
+  'onion',
+  'garlic',
+  'ginger',
+  'carrot'
+]
+
 export default function Home() {
   return (
     <Box
-      width="100vh" 
+      width="100vw" 
       height="100vh"
       display={'flex'}
       justifyContent={'center'}
       alignItems={'center'}
-    >Hello World!</Box>
+    ></Box>
   )
 }
