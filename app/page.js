9c0b0ec04e82403ab0668901928682c8a7c6aa96
loisemusungu <mwongelimusungu@gmail.com>
2024-07-31 09:09:36 +0300

    added a border and title

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 const item = [
   "tomato",
@@ -21,22 +21,42 @@ export default function Home() {
       display={"flex"}
       justifyContent={"center"}
       alignItems={"center"}
+      flexDirection={"column"}
     >
-      <Stack width="800px" height="200px" spacing={2} overflow={"auto"}>
-        {item.map((i) => (
-          <Box
-            key={i}
-            width="100%"
-            height="100px"
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            bgcolor={"#f0f0f0"}
-          >
-            {i}
-          </Box>
-        ))}
-      </Stack>
+      <Box border={"1px solid #333"}>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor={"#EBDDC3"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
+            Pantry Items
+          </Typography>
+        </Box>
+        <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
+          {item.map((i) => (
+            <Box
+              key={i}
+              width="100%"
+              height="300px"
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              bgcolor={"#f0f0f0"}
+            >
+              <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
+                {
+                  // capitalize first letter of the item
+                  i.charAt(0).toUpperCase() + i.slice(1)
+                }
+              </Typography>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
     </Box>
   );
 }
