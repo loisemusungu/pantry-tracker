f25a025160b2a83b43917f0b0204e0a69a246355
loisemusungu <mwongelimusungu@gmail.com>
2024-08-02 07:24:53 +0300

    added the add button and modal

--- app/page.js
+++ app/page.js
@@ -1,13 +1,29 @@
 "use client"
 
 import { useEffect, useState } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, Button, Modal } from "@mui/material";
 import { firestore } from "@/firebase";
 import { collection, getDocs, query } from "@firebase/firestore";
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #333',
+  boxShadow: 24,
+  p: 4,
+}
 
 export default function Home() {
   const [pantryTracker, setPantryTracker] = useState([])
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   useEffect(() => {
     const updatePantryTracker = async () => {
       const snapshot = query(collection(firestore, "pantryTracker"))
@@ -29,8 +45,27 @@ export default function Home() {
       justifyContent={"center"}
       alignItems={"center"}
       flexDirection={"column"}
+      gap={2}
     >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
+      <Button variant="contained" onClick={handleOpen}>Add</Button>
       <Box border={"1px solid #333"}>
+        
         <Box
           width="800px"
           height="100px"
@@ -48,7 +83,7 @@ export default function Home() {
             <Box
               key={i}
               width="100%"
-              height="300px"
+              minHeight="150px"
               display={"flex"}
               justifyContent={"center"}
               alignItems={"center"}
